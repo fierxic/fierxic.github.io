62357efc72271c53cb9862fe11f9c96e752d3d25
fierxic <shafa.bs25@gmail.com>
2024-06-22 10:34:21 +0700

    Update fierxic.js

--- fierxic.js
+++ fierxic.js
@@ -28,8 +28,6 @@ document.querySelectorAll('nav a').forEach(anchor => {
     });
 });
 
-// Get the button:
-let mybutton = document.getElementById("backToTopBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
@@ -49,22 +47,22 @@ mybutton.onclick = function() {
 }
 
 // Dynamic Greeting
-function getGreeting() {
-    const now = new Date();
-    const hours = now.getHours();
-    let greeting;
-    if (hours < 12) {
-        greeting = "Good Morning!";
-    } else if (hours < 18) {
-        greeting = "Good Afternoon!";
-    } else {
-        greeting = "Good Evening!";
-    }
-    console.log("Current Time: " + now);
-    console.log("Greeting: " + greeting);
-    document.getElementById('greeting').textContent = greeting;
-}
-document.addEventListener('DOMContentLoaded', getGreeting);
+// function getGreeting() {
+//     const now = new Date();
+//     const hours = now.getHours();
+//     let greeting;
+//     if (hours < 12) {
+//         greeting = "Good Morning!";
+//     } else if (hours < 18) {
+//         greeting = "Good Afternoon!";
+//     } else {
+//         greeting = "Good Evening!";
+//     }
+//     console.log("Current Time: " + now);
+//     console.log("Greeting: " + greeting);
+//     document.getElementById('greeting').textContent = greeting;
+// }
+// document.addEventListener('DOMContentLoaded', getGreeting);
 
 // Form Validation
 document.getElementById('contactForm').addEventListener('submit', function(e) {
@@ -93,3 +91,30 @@ function validateEmail(email) {
     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return re.test(String(email).toLowerCase());
 }
+
+var imgs = document.querySelectorAll('.slider img');
+var dots = document.querySelectorAll('.dot');
+var currentImg = 0; // index of the first image 
+const interval = 5000; // duration(speed) of the slide
+
+function changeSlide(n) {
+  for (var i = 0; i < imgs.length; i++) { // reset
+    imgs[i].style.opacity = 0;
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+
+  currentImg = (currentImg + 1) % imgs.length; // update the index number
+
+if (n != undefined) {
+    clearInterval(timer);
+    timer = setInterval(changeSlide, interval);
+    currentImg = n;
+}
+
+  imgs[currentImg].style.opacity = 1;
+  dots[currentImg].className += ' active';
+
+
+}
+var timer = setInterval(changeSlide, interval);
+
